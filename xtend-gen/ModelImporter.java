fbb23179499dcaeea4154a0ab3e8254be804a7fb
ginetrix <voskobojnikov.artemij@gmail.com>
2017-06-15 22:26:59 +0200

    cleanup

--- xtend-gen/ModelImporter.java
+++
@@ -1,8 +0,0 @@
-import org.eclipse.xtext.xbase.lib.InputOutput;
-
-@SuppressWarnings("all")
-public class ModelImporter {
-  public static void main(final String[] args) {
-    InputOutput.<String>println("Hello World!");
-  }
-}
